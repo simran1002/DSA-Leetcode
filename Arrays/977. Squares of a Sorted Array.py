4486093ebbdbf38623f47c9802cc28e7d1b3cced
Simran Yadav <simranyadav464@gmail.com>
2024-03-02 21:38:54 +0530

    Squares of a Sorted Array

--- Arrays/977. Squares of a Sorted Array.py
+++ Arrays/977. Squares of a Sorted Array.py
@@ -13,3 +13,51 @@ class Solution:
 
 # Time Complexity : O(nlogn)
 # Space Complexity : O(1)
+
+
+
+
+
+# class Solution {
+# public:
+#     vector<int> sortedSquares(vector<int>& nums) {
+#         for(int i = 0; i < nums.size(); i++) {
+#             nums[i] = (nums[i] * nums[i]);
+#         }
+
+#         sort(begin(nums), end(nums));
+
+#         return nums;
+#     }
+# };
+
+
+
+
+
+# class Solution {
+# public:
+#     vector<int> sortedSquares(vector<int>& nums) {
+#         int n = nums.size();
+
+#         vector<int>result(n);
+#         int i = 0,j=n-1;
+#         int k=n-1;
+#         while(k>=0){
+#             int a = nums[i]*nums[i];
+#             int b = nums[j]*nums[j];
+
+#             if(a>b) {
+#                 result[k]=a;
+#                 i++;
+#             } else {
+#                 result[k]=b;
+#                 j--;
+#             }
+
+#             k--;
+#         }
+
+#         return result;
+#     }
+# };
