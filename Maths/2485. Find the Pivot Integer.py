a4f85b3041e5d0b7e5f8aadb3079b3d6f2a694d2
Simran_Yadav <simranyadav464@gmail.com>
2024-03-13 12:27:29 +0530

    Find the Pivot Integer

---
+++ Maths/2485. Find the Pivot Integer.py
@@ -0,0 +1,18 @@
+'''
+Given a positive integer n, find the pivot integer x such that:
+The sum of all elements between 1 and x inclusively equals the sum of
+ all elements between x and n inclusively.
+Return the pivot integer x. If no such integer exists, return -1.
+ It is guaranteed that there will be at most one pivot index for the given input.
+'''
+
+class Solution:
+    def pivotInteger(self, n: int) -> int:
+        x=sqrt(n*(n+1)/2)
+        if x==int(x):
+            return int(x) 
+        else:
+            return -1
+        
+Time Complexity: O(logn)
+Space Complexity: O(1)
