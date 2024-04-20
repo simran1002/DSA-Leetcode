ead528fe36b55bdcc41705936e545c67849be226
SIMRAN YADAV <104621644+simran1002@users.noreply.github.com>
2024-04-20 20:09:55 +0530

    Time: 782 ms (82.50%) | Memory: 31.1 MB (51.00%) - LeetSync

---
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findFarmland(self, land: list[list[int]]) -> list[list[int]]:
+        m = len(land)
+        n = len(land[0])
+        res = []
+        
+        def exploreFarmland(r1, c1):
+            r2, c2 = r1, c1
+            
+            while r2 < m and land[r2][c1] == 1:
+                c2 = c1
+                while c2 < n and land[r2][c2] == 1:
+                    land[r2][c2] = 0
+                    c2 += 1
+                r2 += 1
+                
+            return [r1, c1, r2 - 1, c2 - 1]
+        
+        for r1 in range(m):
+            for c1 in range(n):
+                if land[r1][c1] == 1:
+                    coordinates = exploreFarmland(r1, c1)
+                    res.append(coordinates)
+        
+        return res
