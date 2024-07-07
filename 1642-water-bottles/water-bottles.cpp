2ac3b8b58b180d21a716f27ac9f6a5be61c75e31
SIMRAN YADAV <104621644+simran1002@users.noreply.github.com>
2024-07-08 01:38:28 +0530

    Time: 3 ms (13.73%) | Memory: 7 MB (72.67%) - LeetSync

--- 1642-water-bottles/water-bottles.cpp
+++ 1642-water-bottles/water-bottles.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
-        int consumed = 0;
-        while(numBottles >= numExchange) {
-            consumed += numExchange;
-            numBottles -= numExchange;
+        // int consumed = 0;
+        // while(numBottles >= numExchange) {
+        //     consumed += numExchange;
+        //     numBottles -= numExchange;
 
-            numBottles += 1;
+        //     numBottles += 1;
+        // }
+        // return consumed + numBottles;
+        int consumed = numBottles;
+        int emptyBottles = numBottles;
+
+        while( emptyBottles >= numExchange) {
+            int extraFullBottles = emptyBottles / numExchange;
+            int remain = emptyBottles % numExchange;
+            consumed += extraFullBottles;
+            emptyBottles = remain + extraFullBottles;
         }
-        return consumed + numBottles;
+
+        return consumed;
     }
 };
