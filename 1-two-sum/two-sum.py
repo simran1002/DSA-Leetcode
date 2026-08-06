9d21c56b7ddac1d9ddcdc062a033665c9b3d34f2
Simran Yadav <104621644+simran1002@users.noreply.github.com>
2026-08-07 01:22:23 +0530

    Time: 3 ms (53.80%) | Memory: 20.5 MB (40.54%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,11 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        D=dict()
-        for i in range(len(nums)):
-            diff = target-nums[i]
-            if diff in D:
-                return [D[diff],i]
-            D[nums[i]]=i
-        
-
+        hashmap ={}
+        for i , num in enumerate(nums):
+            need = target - num
+            if need in hashmap:
+                return [hashmap[need],i]
+            hashmap[num]=i
         
